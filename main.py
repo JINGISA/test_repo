98201ab7d978f76a9472d99d640ebe75523b3807
BaeSangMin <bsm7210@naver.com>
2022-09-28 10:45:07 +0900

    .

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('SMBE')
-    print("add_by_brch3_repo")
+    print("add_by_brch3_repo")asfasdf
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
