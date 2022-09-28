7bacceb354afee9e39dcaeff4129d4d38febc796
JINGISA <89522806+JINGISA@users.noreply.github.com>
2022-09-28 10:39:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,5 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('SMBE')
+    print("add_by_brch3_repo")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
